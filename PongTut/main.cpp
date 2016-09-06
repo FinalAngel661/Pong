a0c4d5103178522610c2279645a43f0c8d887cc6
kelsey.davis <kelsey.davis@students.aie.edu.au>
2016-09-06 15:48:39 -0700

    dhcjdj

--- PongTut/main.cpp
+++ PongTut/main.cpp
@@ -3,7 +3,7 @@
 #include <conio.h>
 //#include "sfwdraw.h"
 using namespace std;
-
+//
 enum eDir {STOP = 0, LEFT = 1, UPLEFT = 2, DOWNLEFT = 3, RIGHT = 4, UPRIGHT = 5, DOWNRIGHT = 6};
 
 class cBall
